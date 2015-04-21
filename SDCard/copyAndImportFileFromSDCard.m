37cbe7f9114ff8daa9e03950f88fce645571ed95
tosiek12 <antek.trad@gmail.com>
2015-04-21 15:03:19 +0200

    Error handling during importing csv from sd card, Script for visualistion of orientation - exporting to bmp files, two new dataset of experiments.

--- SDCard/copyAndImportFileFromSDCard.m
+++ SDCard/copyAndImportFileFromSDCard.m
@@ -1,5 +1,22 @@
-function [imuDatatset] = copyAndImportFileFromSDCard(datasetFileName) 
-    imuDatatset = importfile('E:\\IMU.CSV', 2, inf);
-    copyfile('E:\\IMU.csv',pwd);
-    save(datasetFileName,'imuDatatset');
+function [imuDataset] = copyAndImportFileFromSDCard(datasetFileName) 
+    out = dir([datasetFileName,'.mat']);
+    try
+        if out.name 
+            disp ('Plik o takiej nazwie istnieje, zmien nazwe.')
+            imuDataset = '';
+        else
+            throw('normalna akcja');
+        end
+    catch err
+        disp err
+        copyfile('E:\\IMU.csv', pwd);
+        disp ('Skopiowano plik IMU.csv do working directory.')
+        
+        imuDataset = importfile('IMU.CSV', 2, inf);
+        kolStr = int2str(size(imuDataset,2));
+        disp (['Zaimportowano ', kolStr , ' kolumn']);
+        
+        save(datasetFileName,'imuDataset');
+        disp(['Stworzono plik', datasetFileName, '.mat']);
+    end
 end
--- SDCard/showImuAngleData.m
+++ SDCard/showImuAngleData.m
@@ -2,14 +2,32 @@
 clc;
 clear all;
 close all
-IMU = copyAndImportFileFromSDCard('imuRotationOfPitchAngle');
-%% Compute time vector
+%%
+IMU = copyAndImportFileFromSDCard('imuUpAndDownInOZ2');
+%% (optional) Just load only file to workspace:
+% load('imuRotationOfPitchAngle.mat');
+load('imuUpAndDownInOZ2.mat');
+IMU = imuDataset;
+clear imuDataset;
+%% (optional) Skip some first few data records
+numberRecordToSkip = 50;
+if numberRecordToSkip < length(IMU) 
+    if ~exist('IMU_org') 
+        IMU_org = IMU; 
+    end
+    IMU = IMU_org((numberRecordToSkip:length(IMU_org)),:);
+    disp 'Obcieto'
+end
+%% Plot all angles
+% Compute time vector for ploting
 numberOfProbes = length(IMU);
 undersamplingFactor = 40;
 samplingFrequency = 400;    %Hz
 loggingFrequency = samplingFrequency/undersamplingFactor;   
 t = (1:numberOfProbes)./loggingFrequency;
-%% Plot roll:
+% get some values for figure plotting
+scrSize = get(0,'ScreenSize');
+% Plot roll:
 data = [IMU.x,IMU.x_c];
 numberOfPlot = 1;
 IMU_raw = rad2deg(data(:,1)/1000);
@@ -20,8 +38,7 @@ IMU_cooked = rad2deg(data(:,2)/1000);
 m_cooked = mean(IMU_cooked);
 s_cooked = std(IMU_cooked);
 
-figure(1);
-subplot(3,1,numberOfPlot);
+figureHandler = figure('Position',[50, scrSize(4)/2-100,scrSize(3)/2,scrSize(4)/3]);
 plot(t, IMU_raw,'-b',t, IMU_cooked,'-r');
 legend('Raw','Cooked');
 ylim([-180,180]);
@@ -29,9 +46,10 @@ title(sprintf(['%s\nRaw:(mean: %.2f, stdDev: %.2f)\n' ...
     'Cooked:(mean: %.2f, stdDev: %.2f)'],'K¹t Roll' ,m_raw,s_raw,m_cooked,s_cooked));
 xlabel('Czas, s');
 ylabel('K¹t, stopnie');
-%% Plot Yaw:
+saveas(figureHandler,'1roll.bmp');
+
+% Plot pitch:
 data = [IMU.y,IMU.y_c];
-numberOfPlot = 2;
 IMU_raw = rad2deg(data(:,1)/1000);
 m_raw = mean(IMU_raw);
 s_raw = std(IMU_raw);
@@ -40,8 +58,7 @@ IMU_cooked = rad2deg(data(:,2)/1000);
 m_cooked = mean(IMU_cooked);
 s_cooked = std(IMU_cooked);
 
-figure(1);
-subplot(3,1,numberOfPlot);
+figureHandler = figure('Position',[50, scrSize(4)/2-100,scrSize(3)/2,scrSize(4)/3]);
 plot(t, IMU_raw,'-b',t, IMU_cooked,'-r');
 legend('Raw','Cooked');
 ylim([-180,180]);
@@ -49,9 +66,10 @@ title(sprintf(['%s\nRaw:(mean: %.2f, stdDev: %.2f)\n' ...
     'Cooked:(mean: %.2f, stdDev: %.2f)'],'K¹t Pitch' ,m_raw,s_raw,m_cooked,s_cooked));
 xlabel('Czas, s');
 ylabel('K¹t, stopnie');
-%% Plot yaw:
+saveas(figureHandler,'1pitch.bmp');
+
+% Plot yaw:
 data = [IMU.z,IMU.z_c];
-numberOfPlot = 3;
 IMU_raw = rad2deg(data(:,1)/1000);
 m_raw = mean(IMU_raw);
 s_raw = std(IMU_raw);
@@ -60,12 +78,14 @@ IMU_cooked = rad2deg(data(:,2)/1000);
 m_cooked = mean(IMU_cooked);
 s_cooked = std(IMU_cooked);
 
-figure(1);
-subplot(3,1,numberOfPlot);
+figureHandler = figure('Position',[50, scrSize(4)/2-100,scrSize(3)/2,scrSize(4)/3]);
 plot(t, IMU_raw,'-b',t, IMU_cooked,'-r');
 legend('Raw','Cooked');
 ylim([-180,180]);
+xlim('auto');
 title(sprintf(['%s\nRaw:(mean: %.2f, stdDev: %.2f)\n' ...
     'Cooked:(mean: %.2f, stdDev: %.2f)'],'K¹t Yaw' ,m_raw,s_raw,m_cooked,s_cooked));
 xlabel('Czas, s');
 ylabel('K¹t, stopnie');
+saveas(figureHandler,'1yaw.bmp');
+disp 'Koniec';
