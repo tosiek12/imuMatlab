d195162295e3fa0c2409c11fec0fcc58ed7aa14d
tosiek12 <antek.trad@gmail.com>
2015-04-22 09:31:23 +0200

    Adding visualisation of pure imu data, some new plots, some new datasets of experiments.

---
+++ SDCard/showImuData.m
@@ -0,0 +1,60 @@
+%% Close previous elements, import file from SD Card.
+clc;
+clear all;
+close all
+IMU = copyAndImportFileFromSDCard('imuGpsWalkInTheStreet');
+%% (optional) Just load only file to workspace:
+% load('imuRotationOfPitchAngle.mat');
+% load('imuUpAndDownInOZ.mat');
+load('imuGpsWalkInTheStreet.mat');
+IMU = imuDataset;
+clear imuDataset;
+%% Show deltas
+%Compute time vector:
+numberOfProbes = length(IMU);
+undersamplingFactor = 40;
+samplingFrequency = 400;    %Hz
+loggingFrequency = samplingFrequency/undersamplingFactor;   
+t = (1:numberOfProbes)./loggingFrequency;
+mask = or(IMU.x>0,IMU.x<=0);
+% get some values for figure plotting
+scrSize = get(0,'ScreenSize');
+%% Remove incorrect data:
+latCracow = 5.0045743e+07;  %accurate
+lonCracow = 1.9552048e+07;  %accurate
+latCracowMean = 5.340621965851671e+07;  %average
+lonCracowMean = 1.821655450366748e+07;  %average
+distThreshold =  4.0e+6;
+distMean = sqrt((IMU.latG-latCracowMean).^2+(IMU.lonG-lonCracowMean).^2);
+dist = sqrt((IMU.latG-latCracow).^2+(IMU.lonG-lonCracow).^2);
+mask = dist<distThreshold;
+latShow = IMU.latG(mask);
+lonShow = IMU.lonG(mask);
+%% Show gps coordinates: 
+figureHandler = figure('Position',[50, scrSize(4)/2-100, scrSize(3)/2-50, scrSize(4)/2]);
+p = plot(IMU.lonG(mask), IMU.latG(mask), 'xr','MarkerSize',10 );
+hold on
+plot(lonCracow, latCracow, 'og', 'MarkerSize',10);
+xlabel('lonG');
+ylabel('latG');
+title('koordynaty');
+grid on
+axis square
+
+%% Show deltas and speed
+clc
+tShow = t(mask);
+
+figureHandler = figure('Position',[scrSize(3)/2, scrSize(4)/2-100, scrSize(3)/2-50, scrSize(4)/2]);
+data = [IMU.dx(mask)/1000, IMU.dy(mask)/1000, IMU.dz(mask)/1000];
+m_data = mean(data);
+s_data = std(data);
+plot(tShow, data');
+xlabel('Czas, s');
+ylabel('Przyrosty, m');
+legend('dx','dy','dz');
+disp (['mean:',mat2str(m_data,2)]);
+disp (['std:',mat2str(s_data,2)]);
+title(sprintf(['Przyrosty po³o¿enia w kolejnych osiach\n' ...
+    'mean:%s\n' ...
+    'std:%s'],mat2str(m_data,2),mat2str(s_data,2)));
--- SDCard/showImuGPSData.m
+++ SDCard/showImuGPSData.m
@@ -2,10 +2,11 @@
 clc;
 clear all;
 close all
-IMU = copyAndImportFileFromSDCard('imuUpAndDownInOZ');
+IMU = copyAndImportFileFromSDCard('imuGpsWalkInTheGarden');
 %% (optional) Just load only file to workspace:
 % load('imuRotationOfPitchAngle.mat');
-load('imuUpAndDownInOZ.mat');
+% load('imuUpAndDownInOZ.mat');
+load('imuGpsWalkInTheGarden.mat');
 IMU = imuDataset;
 clear imuDataset;
 %% Show Coordinates and height
@@ -18,60 +19,77 @@ t = (1:numberOfProbes)./loggingFrequency;
 
 % get some values for figure plotting
 scrSize = get(0,'ScreenSize');
-
-% Show gps coordinates: 
+%% Remove incorrect data:
+latCracow = 5.0045743e+07;  %average
+lonCracow = 1.9552048e+07;  %average
+latCracowMean = 5.340621965851671e+07;  %average
+lonCracowMean = 1.821655450366748e+07;  %average
+distThreshold =  4.0e+6;
+distMean = sqrt((IMU.latG-latCracowMean).^2+(IMU.lonG-lonCracowMean).^2);
+dist = sqrt((IMU.latG-latCracow).^2+(IMU.lonG-lonCracow).^2);
+mask = dist<distThreshold;
+latShow = IMU.latG(mask);
+lonShow = IMU.lonG(mask);
+tShow = t(mask);
+%% Show gps coordinates: 
 figureHandler = figure('Position',[50, scrSize(4)/2-100, scrSize(3)/2-50, scrSize(4)/2]);
-p = plot(IMU.lonG, IMU.latG, 'xr' );
-set(p,'Color','red','MarkerSize',8);
+p = plot(lonShow, latShow, 'xr','MarkerSize',10 );
+hold on
+plot(lonCracow, latCracow, 'og', 'MarkerSize',10);
 xlabel('lonG');
 ylabel('latG');
 title('koordynaty');
 grid on
 axis square
 
-% Show height from gps and from pressure.
+%% Show height from gps and from pressure.
 figureHandler = figure('Position',[scrSize(3)/2, scrSize(4)/2-100, scrSize(3)/2-50, scrSize(4)/2]);
 subplot(3,1,1)
-plot(t,IMU.altG/10);
+plot(tShow, IMU.altG(mask)/10);
 title('Wysokoœæ z GPS');
 xlabel('Czas, s');
 ylabel('Wysokoœæ, m');
 hold on;
-plot(t,220*ones(1,length(t)),'--g');
+plot(tShow,220*ones(1,length(tShow)),'--g');
 legend('GPS','rzeczywista');
-
+%
 subplot(3,1,2)
-plot(t,IMU.hdop,'-r');
+plot(tShow,IMU.hdop(mask),'-r');
 title('HDOP - horizontal degree of precision');
 xlabel('Czas, s');
 ylabel('Wartoœæ');
 hold on;
-plot(t,4*ones(1,length(t)),'--g');
+plot(tShow,90*ones(1,length(tShow)),'--g');
 legend('HDOP','próg');
 
 subplot(3,1,3)
-plot(t,IMU.altP/1000,'-r');
+plot(tShow, IMU.altP(mask)/1000,'-r');
 title('Wysokoœæ z Ciœnieniomierza');
 xlabel('Czas, s');
 ylabel('Wysokoœæ, m');
 hold on;
-plot(t,220*ones(1,length(t)),'--g');
+plot(tShow, 220*ones(1,length(tShow)),'--g');
 legend('Ciœnieniomierz','rzeczywista');
 
 %% gps on maps
-load('IMUwPokoju.mat');
-IMU = IMwpookoju;
-clear IMwpookoju;
 mapData = [IMU.lonG, IMU.latG, IMU.altG];
 coordinates  = [IMU.lonG, IMU.latG];
 
+% Adding some random gauss noise:
 noise = 1+0.00002.*randn(size(coordinates));
 
+% Converting to dd.(rest) format:
 coordinates_noise = coordinates.*noise;
 coordinates_noise = coordinates_noise.*0.000001;
-
+% Converting to decimal degrees:
 coordinates_noise(:,1) = coordinates_noise(:,1)+ 0.36;  %tak na pa³e, by sie zgadzalo ;)
 coordinates_noise(:,2) = coordinates_noise(:,2)+ 0.03;
 
-lon = coordinates_noise(:,1);
-lat = coordinates_noise(:,2);
+% Creating two vectors for visualisation on map:
+lon_x = coordinates_noise(:,1);
+lat_y = coordinates_noise(:,2);
+%
+figure(1);
+plot(lon_x,lat_y);
+figure(2);
+mapshow(lon_x, lat_y);
